b88ead503dfdc0e3f294a04a22a622911ac13502
YuNan <yunan.hlju@gmail.com>
2017-09-23 20:10:17 +0800

    svm

--- perceptron.py
+++ perceptron.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy
 import random
 import sys
 
 MAX=100
 MIN=0
 POINT_NUM=20
-step=1
+step=0.01
+C = 0.1
 
 class Point:
     def __init__(self):
@@ -43,15 +45,15 @@ class Point3:
 
 class Point4:
     def __init__(self):
-        self.x = random.gauss(100, 10)
-        self.y = random.gauss(100, 10)
+        self.x = random.gauss(90, 10)
+        self.y = random.gauss(90, 10)
 
         self.label = 1
         self.color = 'b'
 class Line:
     def __init__(self):
-        self.x1 = random.randint(MIN, MAX)
-        self.x2 = random.randint(MIN, MAX)
+        self.x1 = random.randint(MIN, MAX//2)
+        self.x2 = random.randint(MAX//2, MAX)
         self.y1 = 0
         self.y2 = 100
 
@@ -72,35 +74,57 @@ class Line:
         self.y = [self.y1, self.y2]
 
 
+def initialPoint():
+    plt.figure()
+    all_point = []
+    for idx in range(POINT_NUM):
+        p = Point3()
+        plt.plot(p.x, p.y, p.color + 'o', label="point")
+        all_point.append(p)
 
-plt.figure()
-all_point = []
-for idx in range(POINT_NUM):
-    p = Point3()
-    plt.plot(p.x, p.y, p.color + 'o', label="point")
-    all_point.append(p)
+    for idx in range(POINT_NUM):
+        p = Point4()
+        plt.plot(p.x, p.y, p.color + 'o', label="point")
+        all_point.append(p)
+    return all_point
 
-for idx in range(POINT_NUM):
-    p = Point4()
-    plt.plot(p.x, p.y, p.color + 'o', label="point")
-    all_point.append(p)
-l = Line()
-plt.plot(l.x, l.y, 'g--', linewidth=1)
-
-Flag = True
-while True:
+def preceptron_base_dis(all_points):
+    line = Line()
+    plt.plot(line.x, line.y, 'g--', linewidth=1)
     Flag = True
-    for p in all_point:
-        if l.sign(p) <= 0:
-            l.w1 +=  step * p.label * p.x
-            l.w2 += step * p.label * p.y
-            l.b += step * p.label
-            Flag = False
-    if Flag:
-        break
-    l.update()
-    #plt.plot(l.x, l.y, 'y--', linewidth=1)
-plt.plot(l.x, l.y, 'o-', linewidth=1)
-plt.show()
+    while True:
+        Flag = True
+        for point in all_points:
+            if line.sign(point) < 1:
+                line.w1 = (1 - step) * line.w1 + step * C * point.label * point.x
+                line.w2 = (1 - step) * line.w2 + step * C * point.label * point.y
+                line.b = line.b + step * C *  point.label
+                Flag = False
+        if Flag:
+            break
+        line.update()
+        #plt.plot(l.x, l.y, 'y--', linewidth=1)
+    plt.plot(line.x, line.y, '.-', linewidth=1)
+    plt.show()
 
+def preceptron(all_points):
+    line = Line()
+    plt.plot(line.x, line.y, 'g--', linewidth=1)
+    Flag = True
+    while True:
+        Flag = True
+        for point in all_points:
+            if line.sign(point) <= 0:
+                line.w1 +=  step * point.label * point.x
+                line.w2 += step * point.label * point.y
+                line.b += step * point.label
+                Flag = False
+        if Flag:
+            break
+        line.update()
+        #plt.plot(line.x, line.y, 'y--', linewidth=1)
+    plt.plot(line.x, line.y, 'o-', linewidth=1)
+    plt.show()
 
+all_points = initialPoint()
+preceptron_base_dis(all_points)
